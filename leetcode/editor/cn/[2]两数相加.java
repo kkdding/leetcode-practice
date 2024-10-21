d4dee75903bc9f6330b1ffa09384332436ed3959
wmding <2386585789@qq.com>
2024-10-22 01:21:30 +0800

    leetcode 2 update

--- leetcode/editor/cn/[2]两数相加.java
+++ leetcode/editor/cn/[2]两数相加.java
@@ -55,28 +55,22 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode pre = new ListNode(0);
-        ListNode cur = pre;
-        int carry = 0;
-        while (l1 != null || l2 != null) {
-            int x = l1 == null ? 0 : l1.val;
-            int y = l2 == null ? 0 : l2.val;
-            int sum = x + y + carry;
+        ListNode dummy = new ListNode(0);
+        ListNode node = dummy;
+        int carrier = 0;
 
-            carry = sum / 10;
-            sum = sum % 10;
-            cur.next = new ListNode(sum);
+        while (l1 != null || l2 != null || carrier != 0) {
+            int sum = (l1 != null ? l1.val : 0) + (l2 != null ? l2.val : 0) + carrier;
 
-            cur = cur.next;
-            if (l1 != null)
-                l1 = l1.next;
-            if (l2 != null)
-                l2 = l2.next;
+            node.next = new ListNode(sum % 10);
+            node = node.next;
+
+            carrier = sum / 10;
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
         }
-        if (carry == 1) {
-            cur.next = new ListNode(carry);
-        }
-        return pre.next;
+
+        return dummy.next;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
