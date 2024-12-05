1412a45bd4e20215ffd7dae1ef8ee868a006d133
kkdding <2386585789@qq.com>
2024-12-05 15:18:23 +0800

    leetcode 15 双指针

---
+++ leetcode/editor/cn/[15]三数之和.go
@@ -0,0 +1,94 @@
+//给你一个整数数组 nums ，判断是否存在三元组 [nums[i], nums[j], nums[k]] 满足 i != j、i != k 且 j !=
+//k ，同时还满足 nums[i] + nums[j] + nums[k] == 0 。请你返回所有和为 0 且不重复的三元组。
+//
+// 注意：答案中不可以包含重复的三元组。
+//
+//
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：nums = [-1,0,1,2,-1,-4]
+//输出：[[-1,-1,2],[-1,0,1]]
+//解释：
+//nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0 。
+//nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0 。
+//nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0 。
+//不同的三元组是 [-1,0,1] 和 [-1,-1,2] 。
+//注意，输出的顺序和三元组的顺序并不重要。
+//
+//
+// 示例 2：
+//
+//
+//输入：nums = [0,1,1]
+//输出：[]
+//解释：唯一可能的三元组和不为 0 。
+//
+//
+// 示例 3：
+//
+//
+//输入：nums = [0,0,0]
+//输出：[[0,0,0]]
+//解释：唯一可能的三元组和为 0 。
+//
+//
+//
+//
+// 提示：
+//
+//
+// 3 <= nums.length <= 3000
+// -10⁵ <= nums[i] <= 10⁵
+//
+//
+// Related Topics 数组 双指针 排序 👍 7165 👎 0
+
+// leetcode submit region begin(Prohibit modification and deletion)
+package main
+
+import "sort"
+
+func threeSum(nums []int) [][]int {
+	sort.Ints(nums)
+	var result [][]int
+	length := len(nums)
+	if length < 3 {
+		return [][]int{}
+	}
+	for index, value := range nums {
+		if nums[index] > 0 {
+			return result
+		}
+		if index > 0 && nums[index] == nums[index-1] {
+			continue
+		}
+
+		left := index + 1
+		right := length - 1
+		for left < right {
+			sum := value + nums[left] + nums[right]
+			switch {
+			case sum > 0:
+				right--
+			case sum < 0:
+				left++
+			default:
+				result = append(result, []int{nums[index], nums[left], nums[right]})
+				for left < right && nums[left] == nums[left+1] {
+					left++
+				}
+				for left < right && nums[right] == nums[right-1] {
+					right--
+				}
+				left++
+				right--
+			}
+		}
+	}
+	return result
+}
+//leetcode submit region end(Prohibit modification and deletion)
