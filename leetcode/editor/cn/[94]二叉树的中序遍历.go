c57cf5d0a9544d0850ed1868fc39c7ca194c6b71
kkdding <2386585789@qq.com>
2024-12-17 21:10:18 +0800

    leetcode 94 Go实现

---
+++ leetcode/editor/cn/[94]二叉树的中序遍历.go
@@ -0,0 +1,72 @@
+//给定一个二叉树的根节点 root ，返回 它的 中序 遍历 。 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：root = [1,null,2,3]
+//输出：[1,3,2]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root = []
+//输出：[]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：root = [1]
+//输出：[1]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数目在范围 [0, 100] 内 
+// -100 <= Node.val <= 100 
+// 
+//
+// 
+//
+// 进阶: 递归算法很简单，你可以通过迭代算法完成吗？ 
+//
+// Related Topics 栈 树 深度优先搜索 二叉树 👍 2167 👎 0
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+package main
+
+//type TreeNode struct {
+//   Val   int
+//   Left  *TreeNode
+//   Right *TreeNode
+//}
+
+func inorderTraversal(root *TreeNode) (res []int) {
+    var inorder func(node *TreeNode)
+    inorder = func(node *TreeNode) {
+        if node == nil {
+            return
+        }
+        inorder(node.Left)
+        res = append(res, node.Val)
+        inorder(node.Right)
+    }
+    inorder(root)
+    return
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
