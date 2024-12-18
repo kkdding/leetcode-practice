a728550297f15116364688a125029e10d1d54ae2
kkdding <2386585789@qq.com>
2024-12-18 20:46:12 +0800

    leetcode 94 递归 Go实现

--- leetcode/editor/cn/[94]二叉树的中序遍历.go
+++ leetcode/editor/cn/[94]二叉树的中序遍历.go
@@ -1,40 +1,40 @@
-//给定一个二叉树的根节点 root ，返回 它的 中序 遍历 。 
+//给定一个二叉树的根节点 root ，返回 它的 中序 遍历 。
 //
-// 
 //
-// 示例 1： 
-// 
-// 
+//
+// 示例 1：
+//
+//
 //输入：root = [1,null,2,3]
 //输出：[1,3,2]
-// 
 //
-// 示例 2： 
 //
-// 
+// 示例 2：
+//
+//
 //输入：root = []
 //输出：[]
-// 
 //
-// 示例 3： 
 //
-// 
+// 示例 3：
+//
+//
 //输入：root = [1]
 //输出：[1]
-// 
 //
-// 
 //
-// 提示： 
 //
-// 
-// 树中节点数目在范围 [0, 100] 内 
-// -100 <= Node.val <= 100 
-// 
 //
-// 
+// 提示：
 //
-// 进阶: 递归算法很简单，你可以通过迭代算法完成吗？ 
+//
+// 树中节点数目在范围 [0, 100] 内
+// -100 <= Node.val <= 100
+//
+//
+//
+//
+// 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
 //
 // Related Topics 栈 树 深度优先搜索 二叉树 👍 2167 👎 0
 
@@ -50,23 +50,23 @@
 package main
 
 //type TreeNode struct {
-//   Val   int
-//   Left  *TreeNode
-//   Right *TreeNode
+//	Val   int
+//	Left  *TreeNode
+//	Right *TreeNode
 //}
 
 func inorderTraversal(root *TreeNode) (res []int) {
-    var inorder func(node *TreeNode)
-    inorder = func(node *TreeNode) {
-        if node == nil {
-            return
-        }
-        inorder(node.Left)
-        res = append(res, node.Val)
-        inorder(node.Right)
+	order(root, &res)
+	return
+}
+
+func order(node *TreeNode, res *[]int) {
+    if node == nil {
+		return
     }
-    inorder(root)
-    return
+	order(node.Left, res)
+	*res = append(*res, node.Val)
+	order(node.Right, res)
 }
 
 //leetcode submit region end(Prohibit modification and deletion)
