81f05a77320e2113662995423351a466879c954f
kkdding <2386585789@qq.com>
2025-02-03 14:21:37 +0800

    leetcode 35 二分查找 Go实现

---
+++ leetcode/editor/cn/[35]搜索插入位置.go
@@ -0,0 +1,60 @@
+//给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+//
+// 请必须使用时间复杂度为 O(log n) 的算法。
+//
+//
+//
+// 示例 1:
+//
+//
+//输入: nums = [1,3,5,6], target = 5
+//输出: 2
+//
+//
+// 示例 2:
+//
+//
+//输入: nums = [1,3,5,6], target = 2
+//输出: 1
+//
+//
+// 示例 3:
+//
+//
+//输入: nums = [1,3,5,6], target = 7
+//输出: 4
+//
+//
+//
+//
+// 提示:
+//
+//
+// 1 <= nums.length <= 10⁴
+// -10⁴ <= nums[i] <= 10⁴
+// nums 为 无重复元素 的 升序 排列数组
+// -10⁴ <= target <= 10⁴
+//
+//
+// Related Topics 数组 二分查找 👍 2424 👎 0
+
+// leetcode submit region begin(Prohibit modification and deletion)
+package main
+
+func searchInsert(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	var mid int
+	for left <= right {
+		mid = (left + right) / 2
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] < target {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+	return left
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
