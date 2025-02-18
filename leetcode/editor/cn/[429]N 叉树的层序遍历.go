92587748e8cc0b4efd6602944e6396c61e57b739
kkdding <2386585789@qq.com>
2025-02-19 02:25:47 +0800

    leetcode 429 二叉树 Go实现

---
+++ leetcode/editor/cn/[429]N 叉树的层序遍历.go
@@ -0,0 +1,74 @@
+//给定一个 N 叉树，返回其节点值的层序遍历。（即从左到右，逐层遍历）。 
+//
+// 树的序列化输入是用层序遍历，每组子节点都由 null 值分隔（参见示例）。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//
+// 
+//输入：root = [1,null,3,2,4,null,5,6]
+//输出：[[1],[3,2,4],[5,6]]
+// 
+//
+// 示例 2： 
+//
+// 
+//
+// 
+//输入：root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,
+//null,13,null,null,14]
+//输出：[[1],[2,3,4,5],[6,7,8,9,10],[11,12,13],[14]]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树的高度不会超过 1000 
+// 树的节点总数在 [0, 10⁴] 之间 
+// 
+//
+// Related Topics 树 广度优先搜索 👍 483 👎 0
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Children []*Node
+ * }
+ */
+package main
+
+//type Node struct {
+//    Val      int
+//    Children []*Node
+//}
+
+func levelOrder(root *Node) [][]int {
+    if root == nil {
+        return [][]int{}
+    }
+    res := make([][]int, 0)
+    curQueue := []*Node{root}
+
+    for len(curQueue) > 0 {
+        nextQueue := make([]*Node, 0)
+        val := make([]int, 0)
+        for _, node := range curQueue {
+            for _, child := range node.Children {
+                nextQueue = append(nextQueue, child)
+            }
+            val = append(val, node.Val)
+        }
+        res = append(res, val)
+        curQueue = nextQueue
+    }
+    return res
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
