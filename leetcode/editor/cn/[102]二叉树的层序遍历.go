7725a218155fc25612eb76092c7e0921595aa747
kkdding <2386585789@qq.com>
2025-02-18 19:42:34 +0800

    leetcode 102 二叉树 Go实现

---
+++ leetcode/editor/cn/[102]二叉树的层序遍历.go
@@ -0,0 +1,78 @@
+//给你二叉树的根节点 root ，返回其节点值的 层序遍历 。 （即逐层地，从左到右访问所有节点）。 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：root = [3,9,20,null,null,15,7]
+//输出：[[3],[9,20],[15,7]]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root = [1]
+//输出：[[1]]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：root = []
+//输出：[]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数目在范围 [0, 2000] 内 
+// -1000 <= Node.val <= 1000 
+// 
+//
+// Related Topics 树 广度优先搜索 二叉树 👍 2072 👎 0
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+package main
+
+//type TreeNode struct {
+//    Val   int
+//    Left  *TreeNode
+//    Right *TreeNode
+//}
+
+func levelOrder(root *TreeNode) [][]int {
+    if root == nil {
+        return [][]int{}
+    }
+    res := make([][]int, 0)
+    curQueue := []*TreeNode{root}
+    for len(curQueue) > 0 {
+        nextQueue := make([]*TreeNode, 0)
+        val := make([]int, 0)
+        for _, node := range curQueue {
+            val = append(val, node.Val)
+            if node.Left != nil {
+                nextQueue = append(nextQueue, node.Left)
+            }
+            if node.Right != nil {
+                nextQueue = append(nextQueue, node.Right)
+            }
+        }
+        res = append(res, val)
+        curQueue = nextQueue
+    }
+    return res
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
