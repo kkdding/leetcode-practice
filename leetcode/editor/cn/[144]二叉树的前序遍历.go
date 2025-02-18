a83db2fa980003ed9d948b38ac44a84e6c9967bc
kkdding <2386585789@qq.com>
2025-02-18 17:02:49 +0800

    leetcode 144 二叉树 Go实现

---
+++ leetcode/editor/cn/[144]二叉树的前序遍历.go
@@ -0,0 +1,88 @@
+//给你二叉树的根节点 root ，返回它节点值的 前序 遍历。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+// 输入：root = [1,null,2,3] 
+// 
+//
+// 输出：[1,2,3] 
+//
+// 解释： 
+//
+// 
+//
+// 示例 2： 
+//
+// 
+// 输入：root = [1,2,3,4,5,null,8,null,null,6,7,9] 
+// 
+//
+// 输出：[1,2,4,5,6,7,3,8,9] 
+//
+// 解释： 
+//
+// 
+//
+// 示例 3： 
+//
+// 
+// 输入：root = [] 
+// 
+//
+// 输出：[] 
+//
+// 示例 4： 
+//
+// 
+// 输入：root = [1] 
+// 
+//
+// 输出：[1] 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数目在范围 [0, 100] 内 
+// -100 <= Node.val <= 100 
+// 
+//
+// 
+//
+// 进阶：递归算法很简单，你可以通过迭代算法完成吗？ 
+//
+// Related Topics 栈 树 深度优先搜索 二叉树 👍 1313 👎 0
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+package main
+
+//type TreeNode struct {
+//    Val   int
+//    Left  *TreeNode
+//    Right *TreeNode
+//}
+
+func preorderTraversal(root *TreeNode) []int {
+    res := make([]int, 0)
+    if root == nil {
+        return res
+    }
+    res = append(res, root.Val)
+    res = append(res, preorderTraversal(root.Left)...)
+    res = append(res, preorderTraversal(root.Right)...)
+    return res
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
