cef0d06146dc20367845f45a7eada2d1fb1e1ea6
kkdding <2386585789@qq.com>
2025-02-19 11:51:51 +0800

    leetcode 572 二叉树 Go实现

---
+++ leetcode/editor/cn/[572]另一棵树的子树.go
@@ -0,0 +1,78 @@
+//
+// 
+// 给你两棵二叉树 root 和 subRoot 。检验 root 中是否包含和 subRoot 具有相同结构和节点值的子树。如果存在，返回 true ；否则
+//，返回 false 。 
+// 
+// 
+//
+// 二叉树 tree 的一棵子树包括 tree 的某个节点和这个节点的所有后代节点。tree 也可以看做它自身的一棵子树。 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：root = [3,4,5,1,2], subRoot = [4,1,2]
+//输出：true
+// 
+//
+// 示例 2： 
+// 
+// 
+//输入：root = [3,4,5,1,2,null,null,null,null,0], subRoot = [4,1,2]
+//输出：false
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// root 树上的节点数量范围是 [1, 2000] 
+// subRoot 树上的节点数量范围是 [1, 1000] 
+// -10⁴ <= root.val <= 10⁴ 
+// -10⁴ <= subRoot.val <= 10⁴ 
+// 
+//
+// Related Topics 树 深度优先搜索 二叉树 字符串匹配 哈希函数 👍 1121 👎 0
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+package main
+
+//type TreeNode struct {
+//    Val   int
+//    Left  *TreeNode
+//    Right *TreeNode
+//}
+
+func isSubtree(root *TreeNode, subRoot *TreeNode) bool {
+    if root == nil {
+        return false
+    }
+    if isSameTree(root, subRoot) {
+        return true
+    }
+    return isSubtree(root.Left, subRoot) || isSubtree(root.Right, subRoot)
+}
+
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+    if p == nil && q == nil {
+        return true
+    }
+    if p == nil || q == nil {
+        return false
+    }
+    if p.Val == q.Val {
+        return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+    }
+    return false
+}
+//leetcode submit region end(Prohibit modification and deletion)
