8246fa237bb459768b89d311569d8cb87516e389
kkdding <2386585789@qq.com>
2025-02-19 19:11:56 +0800

    leetcode 110 二叉树 Go实现

---
+++ leetcode/editor/cn/[110]平衡二叉树.go
@@ -0,0 +1,72 @@
+//给定一个二叉树，判断它是否是 平衡二叉树 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：root = [3,9,20,null,null,15,7]
+//输出：true
+// 
+//
+// 示例 2： 
+// 
+// 
+//输入：root = [1,2,2,3,3,null,null,4,4]
+//输出：false
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：root = []
+//输出：true
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中的节点数在范围 [0, 5000] 内 
+// -10⁴ <= Node.val <= 10⁴ 
+// 
+//
+// Related Topics 树 深度优先搜索 二叉树 👍 1579 👎 0
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+package main
+
+//type TreeNode struct {
+//    Val   int
+//    Left  *TreeNode
+//    Right *TreeNode
+//}
+
+func isBalanced(root *TreeNode) bool {
+    if root == nil {
+        return true
+    }
+    diff := maxDepth(root.Left) - maxDepth(root.Right)
+    if diff > 1 || diff < -1 {
+        return false
+    }
+    return isBalanced(root.Left) && isBalanced(root.Right)
+}
+
+func maxDepth(root *TreeNode) int {
+    if root == nil {
+        return 0
+    }
+    return max(maxDepth(root.Left), maxDepth(root.Right)) + 1
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
