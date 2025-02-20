34ce7a0e9f5f7d948831907f148779e5c8bd633c
kkdding <2386585789@qq.com>
2025-02-20 14:21:38 +0800

    leetcode 617 二叉树 Go实现

--- leetcode/editor/cn/[654]最大二叉树.go
+++ leetcode/editor/cn/[654]最大二叉树.go
@@ -57,11 +57,11 @@
  */
 package main
 
-type TreeNode struct {
-    Val   int
-    Left  *TreeNode
-    Right *TreeNode
-}
+//type TreeNode struct {
+//    Val   int
+//    Left  *TreeNode
+//    Right *TreeNode
+//}
 
 func constructMaximumBinaryTree(nums []int) *TreeNode {
     if len(nums) == 0 {
