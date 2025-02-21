ef22bdabf645214c1fba24988c75b1a28892d748
kkdding <2386585789@qq.com>
2025-02-21 10:51:04 +0800

    leetcode 236 二叉树 Go实现

--- leetcode/editor/cn/[236]二叉树的最近公共祖先.go
+++ leetcode/editor/cn/[236]二叉树的最近公共祖先.go
@@ -60,18 +60,18 @@ package main
 //}
 
 func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
-    if root == nil || root == p || root == q {
-        return root
-    }
-    left := lowestCommonAncestor(root.Left, p, q)
-    right := lowestCommonAncestor(root.Right, p, q)
-    if left != nil && right != nil {
-        return root
-    }
-    if left != nil {
-        return left
-    }
-    return right
+   if root == nil ||root == p || root == q {
+       return root
+   }
+   left := lowestCommonAncestor(root.Left, p, q)
+   right := lowestCommonAncestor(root.Right, p, q)
+   if left == nil {
+       return right
+   }
+   if right == nil {
+       return left
+   }
+   return root
 }
 
 //leetcode submit region end(Prohibit modification and deletion)
